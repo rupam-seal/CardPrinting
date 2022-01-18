8400e61263585ab4d046aeadeec33c6f4755907f
rupam-seal <rupam.x.seal@gmail.com>
2022-01-18 22:28:40 +0530

    modified code

--- utils/insert_default_value.py
+++ utils/insert_default_value.py
@@ -12,11 +12,11 @@ class InsertDefault:
         # Month abbreviation, day and year
         self.today = date.today()
         self.dateEntry = self.today.strftime("%b-%d-%Y")
-        self.obj.entry_date.insert(0, self.dateEntry)
+        self.obj.entry6.insert(0, self.dateEntry)
 
         # inserting '0.00' to the silver entry
-        self.obj.entry_silver_value.insert(0, "0.00")
+        self.obj.entry11.insert(0, "0.00")
         # inserting '0.00' to the other entry
-        self.obj.entry_other_value.insert(0, "0.00")
+        self.obj.entry13.insert(0, "0.00")
         # inserting '0.00' to the copper entry
-        self.obj.entry_coper_value.insert(0, "0.00")
+        self.obj.entry12.insert(0, "0.00")
